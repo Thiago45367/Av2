e6e2f4ec6c57e7fc3027f365a201ad62845e531b
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-05-10 11:31:17 -0300

    Add files via upload

--- NetBeansProjects/Avaliacao2/src/main/java/controller/PacienteController.java
+++ NetBeansProjects/Avaliacao2/src/main/java/controller/PacienteController.java
@@ -4,10 +4,19 @@
  */
 package controller;
 
-/**
- *
- * @author aluno.den
- */
+import dao.PacienteDAO;
+import model.Paciente;
+import java.sql.SQLException;
+import java.util.List;
+
 public class PacienteController {
-    
+      private PacienteDAO dao = new PacienteDAO();
+
+    public void adicionarPaciente(Paciente paciente) throws SQLException {
+        dao.inserir(paciente);
+    }
+
+    public List<Paciente> listarPacientes() throws SQLException {
+        return dao.listar();
+    }
 }
