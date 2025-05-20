fadd36fb8f761571dc41a5b3141a02461a52f4ff
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2025-05-20 11:45:11 -0300

    Add files via upload

---
+++ NetBeansProjects/Avaliacao2/src/main/java/com/mycompany/avaliacao2/main.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.avaliacao2;
+
+import view.TelaLogin;
+
+
+
+/**
+ *
+ * @author aluno.den
+ */
+public class main {
+
+    public static void main(String[] args) {
+        TelaLogin tela = new TelaLogin();
+        tela.setVisible(true);
+    }
+}
